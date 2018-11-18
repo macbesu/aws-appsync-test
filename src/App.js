9404dc78f3050ef00b7979b3e4c53f59c156eabc
ivanwarke <iveon.cc@gmail.com>
2018-11-19 01:16:05 +0800

    use fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
-export default () => {
-  const [count, setCount] = useState(0);
-  const [person, setPerson] = useState(null);
+const useFetch = url => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(async () => {
-    const res = await fetch('https://api.randomuser.me/');
+    const res = await fetch(url);
     const data = await res.json();
     const [item] = data.results;
-    setPerson(item);
+    setData(item);
+    setLoading(false);
   }, []);
+  
+  return { data, loading };
+}
+
+export default () => {
+  const [count, setCount] = useState(0);
+  const { data, loading } = useFetch("https://api.randomuser.me/");
 
   return (
     <div>
       <div>You clicked {count} times</div>
       <button onClick={() => setCount(count+1)}>Click Me</button>
-      { person && <div>{person.name.first}</div> }
+      { loading ? <div>loading...</div> : <div>{data.name.first}</div> }
     </div>
   ); 
 };
